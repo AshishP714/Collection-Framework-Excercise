d8f4f286e2deb484802bc45424fdf53aab4f64d2
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-11 09:11:48 +0530

    Adding objects into ArrayList

---
+++ Employee.java
@@ -0,0 +1,18 @@
+package java8;
+
+public class Employee {
+
+	public int id;
+	public String name;
+	public String email;
+	public double salary;
+	public String address;
+	
+	public Employee(int id, String name, String email, double salary, String address) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.salary = salary;
+		this.address = address;
+	}
+}
