015f5afc958015692cac2ed13911fb795e098ab5
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-06-26 23:16:35 +0530

    Clean up HashSetPractice.java by removing comments
    Removed comments and adjusted formatting in HashSetPractice.java.

--- HashSetPractice.java
+++ HashSetPractice.java
@@ -5,10 +5,8 @@ public class HashSetPractice
 {
     public static void main(String[] args) 
     {
-        // Create a HashSet of Strings
         HashSet<String> hs = new HashSet<>();
 
-        // Add elements to the HashSet
         hs.add("A");
         hs.add("B");
         hs.add("Geeks");
@@ -16,17 +14,14 @@ public class HashSetPractice
         hs.add("Geeks");
         hs.add("Z");
 
-        // Using iterator() method to iterate Over the HashSet
         System.out.print("Using iterator : ");
         Iterator<String> iterator = hs.iterator();
       
-      	// Traversing HashSet
         while (iterator.hasNext())
             System.out.print(iterator.next() + ", ");
 
         System.out.println();
 
-        // Using enhanced for loop to iterate Over the HashSet
         System.out.print("Using enhanced for loop : ");
         for (String element : hs)
             System.out.print(element + " , ");
