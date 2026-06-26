c3729a83d339bf22a4dd93b8bf6877e24a08ac2a
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-06-26 23:14:25 +0530

    Refactor ArrayList usage and improve output format

--- Practice.java
+++ Practice.java
@@ -2,18 +2,14 @@ import java.util.*;
 public class Practice {
     public static void main(String[] args) {
         
-        // Creating a List of Strings using ArrayList (dynamic array)
         List<String> list = new ArrayList<>();
-        
-        // Adding elements to the ArrayList
+       
         list.add("Java");
         list.add("Python");
         list.add("C++");
-
-        // Printing the elements of the ArrayList
-        System.out.println("Programming Languages:");
         
-        // Enhanced for-loop to iterate through the list
+        System.out.println("Programming Languages:");
+       
         for (String lang : list) {
             System.out.println(lang);
         }
